d7bb1e8113a0e5bfe3c25bbea1ad7e063fd9b7f4
malongqi <malongqi-0409>
2018-11-29 18:20:39 +0800

    实现产品列表

--- src/components/product/index.js
+++ src/components/product/index.js
@@ -1,21 +1,70 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './header.scss'
+import './product.scss'
 export default class Product extends Component {
-	constructor {
+	constructor (props){
 		super(props)
+		this.state = {
+			ProductData: this.props.data
+		}
+		
 	}
 	render () {
 		return (
 			<div className="product-box">
 				<div className="product-item">
 					<div className="shop-infor">
-						<div className="checkbox">
+						<div className="check-box">
+							<input className="checkbox" type="text" type="checkbox" id="radio"/>
+							<label className="radio" for="radio"></label>
 						</div>
 						<div className="shop-title">
-							<i className="shop-icon"></i>
-							<p className="title"></p>
+							<i className={`shop-icon ${this.state.ProductData.shopInfor.type === 'B'? 'shop_B' : ''} ${this.state.ProductData.shopInfor.type === 'C'? 'shop_C' : ''}`}></i>
+							<a href="" className="title">
+								<p>{this.state.ProductData.shopInfor.name}</p>
+								<i className="icon">&#xe6a7;</i>
+							</a>
+							
 						</div>
+						<div className="edit">
+							<p>编辑</p>
+						</div>
+					</div>
+					<div className="product-group">
+						{
+							this.state.ProductData.productList.map(item =>
+								(
+									<div className="group-list">
+										<div className="check-box">
+											<input className="checkbox" type="text" type="checkbox" id="radio"/>
+											<label className="radio" for="radio"></label>
+										</div>
+										<div className="list-detail">
+											<div className="item-img">
+												<a href=""><img src={item.img} alt=""/></a>
+											</div>
+										</div>
+										<div className="list-infor">
+											<a href="">
+												<h3>{item.title}</h3>
+											</a>
+											<div className="select-type">
+												<p>{item.color};{item.size}</p>
+												<i className="icon">&#xe6a6;</i>
+											</div>
+											<div className="list-bottom">
+												<div className="item-price"><span>{item.price}</span></div>
+												<div className="inputnum">
+													<p className='btn num-minus'></p>
+													<p className="btn btn-input"><input type="text" value={item.quantity}/></p>
+													<p className='btn num-add'></p>
+												</div>
+											</div>
+										</div>
+									</div>
+								)
+							)
+						}	
 					</div>
 				</div>
 			</div>
--- src/views/car.js
+++ src/views/car.js
@@ -2,12 +2,77 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './../style/car.scss'
 import MHeader from './../components/header/index.js'
+import Product from './../components/product/index.js'
 
 class Car extends Component {
+	 constructor(props){
+        super(props)
+		this.state = {
+			list: [
+				{
+					shopInfor:{
+						name: '云上生活云上生活云上生活云上生活云上生活云上生活云上生活云上生活云上生活',
+						type: 'B'
+					},
+					productList: [
+						{
+							title: '云上生活 长袖半高领休闲套头毛衣2018新款保暖显瘦打底上衣A9592',
+							img: 'https://gw.alicdn.com/bao/uploaded/i1/39501878/O1CN011PkAHMzdeJ1zqd0_!!39501878.jpg_200x200q50s150.jpg_.webp',
+							price: 190,
+							size:'XL',
+							color: '蓝色',
+							quantity: 1
+						},
+						{
+							title: '云上生活 长袖半高领休闲套头毛衣2018新款保暖显瘦打底上衣A9592',
+							img: 'https://gw.alicdn.com/bao/uploaded/i1/39501878/O1CN011PkAHMzdeJ1zqd0_!!39501878.jpg_200x200q50s150.jpg_.webp',
+							price: 190,
+							size:'XL',
+							color: '蓝色',
+							quantity: 1
+						}
+					]
+				},
+				{
+					shopInfor:{
+						name: '云上生',
+						type: 'C'
+					},
+					productList: [
+						{
+							title: '云上生活 长袖半高领休闲套头毛衣2018新款保暖显瘦打底上衣A9592',
+							img: 'https://gw.alicdn.com/bao/uploaded/i1/39501878/O1CN011PkAHMzdeJ1zqd0_!!39501878.jpg_200x200q50s150.jpg_.webp',
+							price: 190,
+							size:'XL',
+							color: '蓝色',
+							quantity: 1
+						},
+						{
+							title: '云上生活 长袖半高领休闲套头毛衣2018新款保暖显瘦打底上衣A9592',
+							img: 'https://gw.alicdn.com/bao/uploaded/i1/39501878/O1CN011PkAHMzdeJ1zqd0_!!39501878.jpg_200x200q50s150.jpg_.webp',
+							price: 190,
+							size:'XL',
+							color: '蓝色',
+							quantity: 1
+						}
+					]
+				}
+			]
+		}
+	}
+	
 	render () {
 		return (
 			<div className="container">
 				<MHeader title="HEADER" icontext="&#xe697;"></MHeader>
+				<div className="mian">
+				{this.state.list.map(item => (
+						<Product data={item}></Product>
+						)
+					)
+				}
+				
+				</div>
 			</div>
 		)
 	}
